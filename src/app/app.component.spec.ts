07c783811c9e6a9dc39b630245129cd8c5b964bd
SudhirVeerakamaraj <gladiator.sudhir@gmail.com>
2016-12-28 14:31:31 +1100

    Fixed typescript and awesome-typescript loader compatability issue

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,10 @@ import {Headers, Http} from '@angular/Http';
 import { FormsModule }   from '@angular/forms';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {CountryComponent} from './custom.components/country.component/country.component';
-import {ReportCardComponent}from './custom.components/report-card.component/report-card.component';
-import {CountryService}from './custom.components/country.component/country.component.service';
 
 describe('App', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports:[FormsModule], declarations: [AppComponent,CountryComponent,ReportCardComponent], providers:[Http,CountryService]});
+    TestBed.configureTestingModule({ imports:[FormsModule], declarations: [AppComponent], providers:[Http]});
   });
   it ('should work', () => {
     let fixture = TestBed.createComponent(AppComponent);
